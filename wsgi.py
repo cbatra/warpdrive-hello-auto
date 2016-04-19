d8f0515bd9d68c3fff6ed0078d10fc5f575a664c
cbatra <batra2106@gmail.com>
2016-04-18 23:02:46 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 def application(environ, start_response):
     status = '200 OK'
-    output = b'Hello Washington!'
+    output = b'Hello Seattle!'
 
     response_headers = [('Content-type', 'text/plain'),
                         ('Content-Length', str(len(output)))]
